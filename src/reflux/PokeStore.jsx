4fad5be4c369a37966fdbc7ea7b06453caf98c44
shaynemeyer <shayne.meyer@gmail.com>
2016-01-19 06:42:23 -0800

    fixed mispelling in PokeStore

--- src/reflux/PokeStore.jsx
+++ src/reflux/PokeStore.jsx
@@ -2,7 +2,9 @@ var HTTP = require('../services/HttpService.js');
 var Reflux = require('reflux');
 var Actions = require('./Actions.jsx');
 
-var EmailStore = Reflux.createStore({
+// http://pokeapi.co/docs/#pokemon
+
+var PokeStore = Reflux.createStore({
     listenables: [Actions],
     getAllPokemon: function(filter){
         HTTP.post('/pokemon', filter)
@@ -12,4 +14,4 @@ var EmailStore = Reflux.createStore({
     }
 });
 
-module.exports = EmailStore;
+module.exports = PokeStore;
